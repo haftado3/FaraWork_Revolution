2aed7243c52eeaa525df2d20c718e0219f6f0673
Mahdi Khalili <Mahdi Khalili@DESKTOP-9CF4PCU>
2018-08-10 08:09:52 -0700

    Drag & Drop For List View From Josh Smith

---
+++ FaraWork_Revolution/XTask/ViewModels/TaskHolderViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaraWork_Revolution.XTask.ViewModels
+{
+    public class TaskHolderViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private string _title = "";
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+        private ObservableCollection<Entities.XTask> _tasks = new ObservableCollection<Entities.XTask>();
+        public ObservableCollection<Entities.XTask> Tasks
+        {
+            get
+            {
+                return _tasks;
+            }
+            set
+            {
+                _tasks = value;
+                OnPropertyChanged(nameof(Tasks));
+            }
+        }
+    }
+}
--- FaraWork_Revolution/XTask/Views/TaskHolder.xaml.cs
+++ FaraWork_Revolution/XTask/Views/TaskHolder.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using FaraWork_Revolution.XTask.DragNDrop;
+using FaraWork_Revolution.XTask.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,118 @@ namespace FaraWork_Revolution.XTask.Views
     /// </summary>
     public partial class TaskHolder : UserControl
     {
+        DragNDrop.DragNDropManager<XTask.Entities.XTask> dragMgr;
         public TaskHolder()
         {
             InitializeComponent();
+            this.Loaded += TaskHolderLoaded;
         }
+        private ObservableCollection<XTask.Entities.XTask> CreateSimpleTasks()
+        {
+            ObservableCollection<XTask.Entities.XTask> temp = new ObservableCollection<Entities.XTask>();
+            Entities.XTask t = new Entities.XTask();
+            t.Color = new SolidColorBrush(Colors.Yellow);
+            t.Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+            t.ID = Guid.NewGuid();
+
+            Entities.XTask t2 = new Entities.XTask()
+            {
+                Color = new SolidColorBrush(Colors.Red),
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                ID = Guid.NewGuid()
+            };
+            temp.Add(t);
+            temp.Add(t2);
+            return temp;
+        }
+        void TaskHolderLoaded (object sender,RoutedEventArgs e)
+        {
+            (this.DataContext as TaskHolderViewModel).Tasks = CreateSimpleTasks();
+            dragMgr = new DragNDrop.DragNDropManager<Entities.XTask>(this.MlistView);
+            this.MlistView.DragEnter += OnListViewDragEnter;
+            this.MlistView.DragEnter += OnListViewDragEnter;
+            this.MlistView.Drop += OnListViewDrop;
+            this.MlistView.Drop += OnListViewDrop;
+        }
+        #region dragMgr_ProcessDrop
+
+        // Performs custom drop logic for the top ListView.
+        void dragMgr_ProcessDrop(object sender, ProcessDropEventArgs<Task> e)
+        {
+            // This shows how to customize the behavior of a drop.
+            // Here we perform a swap, instead of just moving the dropped item.
+
+            int higherIdx = Math.Max(e.OldIndex, e.NewIndex);
+            int lowerIdx = Math.Min(e.OldIndex, e.NewIndex);
+
+            if (lowerIdx < 0)
+            {
+                // The item came from the lower ListView
+                // so just insert it.
+                e.ItemsSource.Insert(higherIdx, e.DataItem);
+            }
+            else
+            {
+                // null values will cause an error when calling Move.
+                // It looks like a bug in ObservableCollection to me.
+                if (e.ItemsSource[lowerIdx] == null ||
+                    e.ItemsSource[higherIdx] == null)
+                    return;
+
+                // The item came from the ListView into which
+                // it was dropped, so swap it with the item
+                // at the target index.
+                e.ItemsSource.Move(lowerIdx, higherIdx);
+                e.ItemsSource.Move(higherIdx - 1, lowerIdx);
+            }
+
+            // Set this to 'Move' so that the OnListViewDrop knows to 
+            // remove the item from the other ListView.
+            e.Effects = DragDropEffects.Move;
+        }
+
+        #endregion // dragMgr_ProcessDrop
+
+        #region OnListViewDragEnter
+
+        // Handles the DragEnter event for both ListViews.
+        void OnListViewDragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Move;
+        }
+
+        #endregion // OnListViewDragEnter
+
+        #region OnListViewDrop
+
+        // Handles the Drop event for both ListViews.
+        void OnListViewDrop(object sender, DragEventArgs e)
+        {
+            if (e.Effects == DragDropEffects.None)
+                return;
+
+            Task task = e.Data.GetData(typeof(Task)) as Task;
+            if (sender == this.MlistView)
+            {
+                if (this.dragMgr.IsDragInProgress)
+                    return;
+
+                // An item was dragged from the bottom ListView into the top ListView
+                // so remove that item from the bottom ListView.
+                //(this.listView2.ItemsSource as ObservableCollection<Task>).Remove(task);
+            }
+            else
+            {
+                //if (this.dragMgr2.IsDragInProgress)
+                //    return;
+
+                // An item was dragged from the top ListView into the bottom ListView
+                // so remove that item from the top ListView.
+                (this.MlistView.ItemsSource as ObservableCollection<Task>).Remove(task);
+            }
+        }
+
+        #endregion // OnListViewDrop
+
     }
 }
